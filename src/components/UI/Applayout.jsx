a1b255c35ea413f34ac166dbf5777d374322765c
gautam-auc <agautamauc@gmail.com>
2025-05-08 17:43:50 +0530

    Trying to fixing deploye error

--- src/components/UI/Applayout.jsx
+++ src/components/UI/Applayout.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Outlet, useLocation } from 'react-router'
 import { Navbar } from '../UI/Navbar'
-import Home from '../pages/Home'
 import { Footer } from '../UI/Footer'
-
+import Home from '../pages/home'
 
 export const AppLayout = () => {
   const location = useLocation()
@@ -12,7 +11,7 @@ export const AppLayout = () => {
   return (
     <div className='app-layout'>
       <Navbar />
-      {showIndex.includes(location.pathname) ? <Home /> : <Outlet />}
+      {showIndex.includes(location.pathname) ? <Home/> : <Outlet />}
       <Footer />
     </div>
   )
